62581820e054538360883c1234b0c5747f9c6c5c
Jiang Liu <liujiang682@gmail.com>
2015-06-06 12:45:07 +1000

    Added Place command handle

--- Robot/src/main/java/au/com/bgl/command/CommandEnum.java
+++ Robot/src/main/java/au/com/bgl/command/CommandEnum.java
@@ -1,21 +1,28 @@
 package au.com.bgl.command;
 
+import au.com.bgl.common.StringUtils;
+
 /**
  * The all valid robot commands.
  * 
  * @author Jiang Liu
- *
+ * 
  */
 public enum CommandEnum {
 	PLACE, MOVE, LEFT, RIGHT, REPORT, DONOTHING;
-	
+
 	public static CommandEnum fromString(final String commandString) {
+		if ((!StringUtils.isNullOrEmpty(commandString))
+				&& (commandString.toUpperCase().startsWith(PLACE.toString()))) {
+			return PLACE;
+		}
+		
 		for (CommandEnum command : CommandEnum.values()) {
 			if (command.toString().equalsIgnoreCase(commandString)) {
 				return command;
 			}
 		}
-		
+
 		return DONOTHING;
 	}
 }
