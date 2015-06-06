849307930617dc1ec272e1eaf913b169cb895689
Jiang Liu <liujiang682@gmail.com>
2015-06-06 18:16:10 +1000

    Test class

---
+++ Robot/src/test/java/au/com/bgl/command/LeftCommandTest.java
@@ -0,0 +1,57 @@
+package au.com.bgl.command;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import au.com.bgl.interf.Robot;
+import au.com.bgl.interf.RobotTable;
+
+public class LeftCommandTest {
+
+	private LeftCommand testInstance;
+	
+	@Mock
+	private RobotTable mockRobotTable;
+	@Mock
+	private Robot mockRobot;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		this.testInstance = new LeftCommand();
+	}
+	
+	@After
+	public void tearDown() {
+		this.testInstance = null;
+	}
+
+	@Test
+	public void testGetCommand() {
+		assertEquals(CommandEnum.LEFT, this.testInstance.getCommand());
+	}
+	
+	@Test
+	public void testIsValid() {
+		assertTrue(this.testInstance.isValidCommand());
+	}
+	
+	@Test
+	public void testIsLegitimateMoveNullRobot() {
+		assertFalse(this.testInstance.isLegitimateMove(mockRobotTable));
+	}
+	
+	@Test
+	public void testIsLegitimateMove() {
+		when(this.mockRobotTable.getRobot()).thenReturn(mockRobot);
+		assertTrue(this.testInstance.isLegitimateMove(mockRobotTable));
+	}
+}
