39d3cca16cb8f1b6a0a1103e3dcceef4c330795d
Jiang Liu <liujiang682@gmail.com>
2015-06-06 18:20:10 +1000

    update

--- Robot/src/test/java/au/com/bgl/BGLRobotTableTest.java
+++ Robot/src/test/java/au/com/bgl/BGLRobotTableTest.java
@@ -1,5 +1,6 @@
 package au.com.bgl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -8,31 +9,42 @@ import java.util.Scanner;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Matchers;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import au.com.bgl.command.factory.CommandConstructFactory;
 import au.com.bgl.command.interf.Command;
 import au.com.bgl.command.interf.CommandExecuter;
 import au.com.bgl.common.CommonConstants;
+import au.com.bgl.executer.factory.ExecuterConstructFactory;
 import au.com.bgl.interf.Robot;
 import au.com.bgl.interf.RobotTable;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({BGLRobotTable.class})
+@PrepareForTest({BGLRobotTable.class, ExecuterConstructFactory.class, CommandConstructFactory.class})
 public class BGLRobotTableTest {
 	
 	private BGLRobotTable instance;
 	private Scanner scanner;
 	
+	@Mock
+	private CommandExecuter mockExecuter;
+	@Mock
+	private Robot mockRobot;
+	@Mock
+	private Command mockCommand;
+	
 	@Before
 	public void setUp() {
+		MockitoAnnotations.initMocks(this);
 		this.instance = PowerMockito.mock(BGLRobotTable.class);
 		scanner = new Scanner(System.in);
 		PowerMockito.doReturn(scanner).when(this.instance).getScanner();
@@ -65,7 +77,6 @@ public class BGLRobotTableTest {
 	 */
 	@Test
 	public void testRunWithInvalidCommand() {
-		Command mockCommand = Mockito.mock(Command.class);
 		Mockito.doReturn(false).when(mockCommand).isValidCommand();
 		PowerMockito.when(this.instance.getNextCommand(this.scanner)).thenReturn(mockCommand).thenReturn(null);
 		Mockito.doCallRealMethod().when(this.instance).run();
@@ -77,44 +88,16 @@ public class BGLRobotTableTest {
 	
 	/**
 	 * This test case tests the getNextCommand returns a command object
-	 * with illegal move flag set to true. It expected the getRobot method never
-	 * get call.
-	 */
-	@Ignore
-	@Test
-	public void testRunWithIllegalMove() {
-		Command mockCommand = PowerMockito.mock(Command.class);
-		PowerMockito.doReturn(true).when(mockCommand).isValidCommand();
-		PowerMockito.doReturn(false).when(mockCommand).isLegitimateMove(Matchers.any(RobotTable.class));
-		PowerMockito.when(this.instance.getNextCommand(this.scanner)).thenReturn(mockCommand).thenReturn(null);
-		Mockito.doCallRealMethod().when(this.instance).run();
-		
-		this.instance.run();
-		
-		Mockito.verify(this.instance, Mockito.times(CommonConstants.ONE)).getRobot();
-		
-		ArgumentCaptor<RobotTable> robotCaptor = ArgumentCaptor.forClass(RobotTable.class);
-		Mockito.verify(mockCommand).isLegitimateMove(robotCaptor.capture());
-		List<RobotTable> captured = robotCaptor.getAllValues();
-		assertNotNull(captured);
-		assertTrue(captured.size() == CommonConstants.ONE);
-	}
-	
-	/**
-	 * This test case tests the getNextCommand returns a command object
 	 * with legal move flag set to true. It expected the getRobot method 
 	 * get call twice.
 	 */
 	@Test
 	public void testRun() {
-		Command mockCommand = PowerMockito.mock(Command.class);
 		PowerMockito.doReturn(true).when(mockCommand).isValidCommand();
 		PowerMockito.doReturn(true).when(mockCommand).isLegitimateMove(Matchers.any(RobotTable.class));
 		PowerMockito.when(this.instance.getNextCommand(this.scanner)).thenReturn(mockCommand).thenReturn(null);
 		
-		Robot mockRobot = PowerMockito.mock(Robot.class);
 		PowerMockito.doReturn(mockRobot).when(this.instance).getRobot();
-		CommandExecuter mockExecuter = PowerMockito.mock(CommandExecuter.class);
 		PowerMockito.doReturn(mockExecuter).when(this.instance).getExecuter(Matchers.eq(mockCommand), Matchers.eq(mockRobot));
 		
 		Mockito.doCallRealMethod().when(this.instance).run();
@@ -133,5 +116,31 @@ public class BGLRobotTableTest {
 		List<Command> capturedCommands = commandCaptor.getAllValues();
 		assertNotNull(capturedCommands);
 		assertTrue(capturedCommands.size() == CommonConstants.ONE);
+	}
+	
+	@Test
+	public void testGetScanner() {
+		PowerMockito.doCallRealMethod().when(this.instance).getScanner();
+		assertNotNull(this.instance.getScanner());
+	}
+	
+	@Test
+	public void testGetExecuter() throws Exception {
+		PowerMockito.mockStatic(ExecuterConstructFactory.class);
+		PowerMockito.doReturn(mockExecuter).when(ExecuterConstructFactory.class, "contructExecuter", Matchers.any(Command.class), Matchers.any(Robot.class));
+		PowerMockito.doCallRealMethod().when(this.instance).getExecuter(mockCommand, mockRobot);
+		
+		assertEquals(this.mockExecuter, this.instance.getExecuter(mockCommand, mockRobot));
+	}
+	
+	@Test
+	public void testGetNextCommand() throws Exception {
+		Scanner mockScanner = PowerMockito.mock(Scanner.class);
+		PowerMockito.doReturn("abc").when(mockScanner).nextLine();
+		PowerMockito.mockStatic(CommandConstructFactory.class);
+		PowerMockito.doReturn(mockCommand).when(CommandConstructFactory.class, "constructCommand", Matchers.anyString());
+		PowerMockito.doCallRealMethod().when(this.instance, "getNextCommand", mockScanner);
+		
+		assertEquals(mockCommand, this.instance.getNextCommand(mockScanner));
 	}
 }
